54c0d2e97a5a2eb19c567511150396c94e1571e6
julianisaza <julian.isaza@pragma.com.co>
2024-09-20 08:37:55 -0500

    Initial version Java Sample - AppConfig

---
+++ src/main/java/com/amazonaws/samples/appconfig/utils/Security.java
@@ -0,0 +1,25 @@
+package com.amazonaws.samples.appconfig.utils;
+
+import javax.security.cert.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Date;
+
+public class Security {
+    public Certificate getCertificate(File certFile) throws CertificateExpiredException, CertificateNotYetValidException {
+        X509Certificate cert = null;
+        try {
+            InputStream inStream = new FileInputStream(certFile);
+            cert = X509Certificate.getInstance(inStream);
+        } catch (CertificateException e) {
+            throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        cert.checkValidity(new Date());
+        return cert;
+    }
+
+}
